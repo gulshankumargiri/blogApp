6dce230979c5e6af62409afd453607ba1c13e2c4
Gulshan Kumar giri <110718968+gulshankumargiri@users.noreply.github.com>
2025-07-25 07:38:45 +0530

    Update supabase_secret.dart
    Api token must be needed

--- lib/core/secrets/supabase_secret.dart
+++ lib/core/secrets/supabase_secret.dart
@@ -1,4 +1,4 @@
 class AppSecrets {
   static const supabaseUrl = 'https://pshglezsedmldwzvecyl.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzaGdsZXpzZWRtbGR3enZlY3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTA5NTAsImV4cCI6MjA2MzE2Njk1MH0.xtq1EYypbn2cKoOGKsBzdNKCb4NrMRLmtCKQCUBtTIE';
+  static const supabaseAnonKey = '';
 }
